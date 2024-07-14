d39c61b81e71c284e549b4ea448b63ba7af8a75c
Eael Work <eaelspagad@outlook.com>
2024-07-14 19:15:53 +0000

    Error in api link

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,47 @@
 #!/usr/bin/node
 const request = require('request');
-const API_LINK = 'https://swapi-api.alx-tools.com/api/';
+const API_LINK = 'https://swapi-api.alx-tools.com/api';
 
 if (process.argv.length > 2) {
-  request(`${API_LINK}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charURL = JSON.parse(body).characters;
-    const charName = charURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, _, charReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charReqBody).name);
-        });
-      }));
+  const filmId = process.argv[2];
+  const filmUrl = `${API_LINK}/films/${filmId}/`;
 
-    Promise.all(charName)
+  request(filmUrl, (err, _, body) => {
+    if (err) {
+      console.log('Error fetching film:', err);
+      return;
+    }
+
+    let filmData;
+    try {
+      filmData = JSON.parse(body);
+    } catch (parseErr) {
+      console.log('Error parsing film data:', parseErr);
+      return;
+    }
+
+    const charURLs = filmData.characters;
+    const charPromises = charURLs.map(url =>
+      new Promise((resolve, reject) => {
+        request(url, (charErr, __, charBody) => {
+          if (charErr) {
+            reject(charErr);
+          } else {
+            try {
+              const charData = JSON.parse(charBody);
+              resolve(charData.name);
+            } catch (parseErr) {
+              reject(parseErr);
+            }
+          }
+        });
+      })
+    );
+
+    Promise.all(charPromises)
       .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+      .catch(allErr => console.log('Error fetching characters:', allErr));
   });
+} else {
+  console.log('Please provide a film ID as an argument.');
 }
