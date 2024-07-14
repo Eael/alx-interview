6dc015bcf160bf36d97d792a99f73fc624dccf9b
Eael Work <eaelspagad@outlook.com>
2024-07-14 18:50:29 +0000

    Starwars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const API_LINK = 'https://swapi-api.alx-tools.com/api/';
+
+if (process.argv.length > 2) {
+  request(`${API_LINK}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charURL = JSON.parse(body).characters;
+    const charName = charURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, _, charReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charReqBody).name);
+          });
+        }));
+
+    Promise.all(charName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
